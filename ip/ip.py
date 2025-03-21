83a66fd5f3aa33d0a08016a31cc88715f864f8e7
khaira1g <193151710+khaira1g@users.noreply.github.com>
2025-03-21 20:57:38 +0000

    added a script

---
+++ ip/ip.py
@@ -0,0 +1,12 @@
+import random as rand
+import sys
+import logging
+from colorama import Fore
+
+logger = logging.getLogger(__name__)
+
+def main():
+    print(Fore.RED + "Starting service...")
+
+if __name__ == '__main__':
+    main()
