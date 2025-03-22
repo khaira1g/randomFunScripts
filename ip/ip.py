717b4c52581c0ffd23e08e2a16b6de6f03fa2b03
khaira1g <193151710+khaira1g@users.noreply.github.com>
2025-03-22 12:45:48 +0000

    finished script

--- ip/ip.py
+++ ip/ip.py
@@ -1,12 +1,48 @@
 import random as rand
 import sys
+import time
 import logging
 from colorama import Fore
 
 logger = logging.getLogger(__name__)
 
+def sleep(sec):
+    time.sleep(sec)
+
 def main():
-    print(Fore.RED + "Starting service...")
+    print("Starting service...")
+
+    target = input("Input target phone number: ")
+
+    if any(char.isalpha() for char in target):
+        print("You have to enter an actual phone number.")
+        sys.exit(1)
+    else:
+        print("Setting target phone number to " + target + "...")
+
+        block = 1
+        sleep(3)
+        for i in range(6):
+            print("Block #" + str(block) + " failed to connect.")
+            block = block + 1
+            sleep(1.5)
+        
+        time.sleep(4)
+        print("Connection on Block #" + str(block) + " found!")
+        sleep(2)
+        print("Starting IP finder...")
+
+        delay = rand.choice(
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+            )
+        sleep(delay)
+        one = str(rand.choice(range(1, 99)))
+        two = str(rand.choice(range(1, 99)))
+        three = str(rand.choice(range(1, 300)))
+        four = str(rand.choice(range(1, 300)))
+
+        str(print(one + "." + two + "." + three + "." + four))
+
 
 if __name__ == '__main__':
     main()
